eedc752ff093324c5837996611924bc83b89fc87
NTL-0728 <truongluan280702@luan280702gmail.com>
2024-03-02 17:09:46 +0700

    fix: clear database

--- create.sql
+++
@@ -1,2 +0,0 @@
-create table student (class_name varchar(255), email varchar(255), id varchar(255) not null, name varchar(255), teacher_name varchar(255), primary key (id));
-create table teacher (class_name varchar(255), email varchar(255), id varchar(255) not null, name varchar(255), primary key (id));
